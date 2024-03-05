0e01f4493c0609486383fce9a2da6bea32fcd24d
John <JohnGQuadri@hotmail.com>
2024-03-05 01:23:15 +0000

    added ingredients class and migration

---
+++ ManagementSystem/Migrations/20240304175615_AddIngredient.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIngredient : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ingredient",
+                columns: table => new
+                {
+                    IngredientId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Calories = table.Column<int>(type: "int", nullable: true),
+                    Salt = table.Column<int>(type: "int", nullable: true),
+                    Fat = table.Column<int>(type: "int", nullable: true),
+                    Quantity = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredient", x => x.IngredientId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ingredient");
+        }
+    }
+}
