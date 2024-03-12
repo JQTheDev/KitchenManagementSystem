68fc4eb7cd2813d43e6147ed7966c2d33bb05458
John <JohnGQuadri@hotmail.com>
2024-03-12 23:40:55 +0000

    added update functionaility. not complete

--- ManagementSystem/wwwroot/js/ingredientQuery.js
+++ ManagementSystem/wwwroot/js/ingredientQuery.js
@@ -45,9 +45,38 @@ function populateIngredients(ingredients) {
 
     ingredients.forEach(ingredient => {
         const option = document.createElement('option');
-        option.value = ingredient.ingredientId;
+        option.id = ingredient.ingredientId;
         option.textContent = ingredient.name;
         selectElement.appendChild(option);
     });
 }
 
+function updateIngredient() {
+    const selectElement = document.getElementById("ingredientList")
+    const selectedOption = selectElement.options[selectElement.selectedIndex];
+    const ingredientId = selectedOption.id;
+
+    const endpoint = `https://localhost:44342/api/Ingredients/${ingredientId}`
+
+    fetch(endpoint)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("could not complete request")
+        }
+        response.json()
+    })
+    .then(ingredient => {
+        // Assuming you want to populate fields in your modal, you'd do something like this:
+        document.getElementById('name').textContent = ingredient.name;
+        document.getElementById('calories').value = ingredient.calories;
+        document.getElementById('salt').value = ingredient.salt;
+        document.getElementById('fat').value = ingredient.fat;
+        document.getElementById('quantity').value = ingredient.quantity;
+        document.getElementById('ingredientModal').style.display = 'block';
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+}
+document.getElementById("updateBtn").addEventListener("click", updateIngredient);
