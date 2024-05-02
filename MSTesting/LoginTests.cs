65b6bfa9f6f812ac63fc507936de3531222ec0ba
John <JohnGQuadri@hotmail.com>
2024-05-02 05:31:50 +0100

    comitting Login test to ss on desktop

--- MSTesting/LoginTests.cs
+++ MSTesting/LoginTests.cs
@@ -3,234 +3,161 @@ using ManagementSystem.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
+
 namespace MSTesting
 {
     public class Tests
     {
+        private MyDbContext _context;
+        private LoginController _controller;
+
         [SetUp]
         public void Setup()
         {
-        }
-
-        [Test]
-        public void TestValidCredentials()
-        {
-
-            var options = new DbContextOptionsBuilder<MyDbContext>()
-            .UseInMemoryDatabase(databaseName: "TestDatabase")
-            .Options;
-
-            using (var context = new MyDbContext(options))
-            {
-                
-
-                var controller = new LoginController(context);
-
-                var user = new User
-                {
-                    Username = "Jquadri24",
-                    Password = "JQAccount11"
-                };
-                context.User.Add(new User { Username= "Jquadri24", Password = HashPassword("JQAccount11") });
-                context.SaveChanges();
-
-                var result = controller.Index(user) as ViewResult;
-
-                Assert.IsNotNull(result);
-                Assert.IsNotNull(result.ViewData["Message"]);
-                Assert.AreEqual("Login Successful", result.ViewData["Message"]);
-            }
-        }
-
-        [Test]
-        public void TestInvalidCredentials_UserNotFound()
-        {
             var options = new DbContextOptionsBuilder<MyDbContext>()
                 .UseInMemoryDatabase(databaseName: "TestDatabase")
                 .Options;
 
-            using (var context = new MyDbContext(options))
-            {
-                var controller = new LoginController(context);
-
-                var user = new User
-                {
-                    Username = "NonExistentUser",
-                    Password = "SomePassword"
-                };
-                context.User.Add(new User { Username = "Jquadri24", Password = "JQAccount11" });
-                context.SaveChanges();
-                var result = controller.Index(user) as ViewResult;
-
-                Assert.IsNotNull(result);
-                Assert.IsNotNull(result.ViewData["ErrorMessage"]);
-                Assert.AreEqual("Username does not exist.", result.ViewData["ErrorMessage"]);
-            }
+            _context = new MyDbContext(options);
+            _controller = new LoginController(_context);
         }
+
         [Test]
-        public void TestInvalidCredentials_IncorrectPassword()
+        public async Task TestValidCredentials()
         {
-            var options = new DbContextOptionsBuilder<MyDbContext>()
-                .UseInMemoryDatabase(databaseName: "TestDatabase")
-                .Options;
+            _context.User.Add(new User { Username = "Jquadri24", Password = HashPassword("JQAccount11") });
+            _context.SaveChanges();
 
-            using (var context = new MyDbContext(options))
-            {
-                var controller = new LoginController(context);
+            var user = new User { Username = "Jquadri24", Password = "JQAccount11" };
+            var result = await _controller.Index(user);
 
-                context.User.Add(new User { Username = "User", Password = HashPassword("CorrectPassword") });
-                context.SaveChanges();
-
-                var user = new User
-                {
-                    Username = "User",
-                    Password = "IncorrectPassword"
-                };
-
-                var result = controller.Index(user) as ViewResult;
-
-                Assert.IsNotNull(result);
-                Assert.IsNotNull(result.ViewData["ErrorMessage"]);
-                Assert.AreEqual("Incorrect Password.", result.ViewData["ErrorMessage"]);
-            }
+            Assert.IsInstanceOf<RedirectToActionResult>(result);
+            var redirectResult = result as RedirectToActionResult;
+            Assert.AreEqual("Home", redirectResult.ControllerName);
+            Assert.AreEqual("Index", redirectResult.ActionName);
         }
+
+        [Test]
+        public async Task TestInvalidCredentials_UserNotFound()
+        {
+            var user = new User
+            {
+                Username = "NonExistentUser",
+                Password = "SomePassword"
+            };
+
+            var result = await _controller.Index(user) as ViewResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.ViewData["ErrorMessage"].ToString().Contains("Invalid username or password."));
+        }
+
+        [Test]
+        public async Task TestInvalidCredentials_IncorrectPassword()
+        {
+            _context.User.Add(new User { Username = "User", Password = HashPassword("CorrectPassword") });
+            _context.SaveChanges();
+
+            var user = new User
+            {
+                Username = "User",
+                Password = "IncorrectPassword"
+            };
+
+            var result = await _controller.Index(user) as ViewResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.ViewData["ErrorMessage"].ToString().Contains("Invalid username or password."));
+        }
+
         [Test]
         public void TestDuplicateUsername()
         {
-            var options = new DbContextOptionsBuilder<MyDbContext>()
-                .UseInMemoryDatabase(databaseName: "TestDatabase")
-                .Options;
+            _context.User.Add(new User { Username = "DevProject", Password = "Password" });
+            _context.SaveChanges();
 
-            using (var context = new MyDbContext(options))
+            var user = new User
             {
-                var controller = new LoginController(context);
+                Username = "DevProject",
+                Password = "IncorrectPassword"
+            };
 
-                context.User.Add(new User { Username = "DevProject", Password = "Password" });
-                context.SaveChanges();
+            var result = _controller.Create(user) as ViewResult;
 
-                var user = new User
-                {
-                    Username = "DevProject",
-                    Password = "IncorrectPassword"
-                };
-
-                var result = controller.Create(user) as ViewResult;
-
-                Assert.IsNotNull(result);
-                Assert.IsNotNull(result.ViewData["ErrorMessage"]);
-                Assert.AreEqual("Username already exists. Please use a different username.", result.ViewData["ErrorMessage"]);
-            }
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.ViewData["ErrorMessage"]);
+            Assert.AreEqual("Username already exists. Please use a different username.", result.ViewData["ErrorMessage"]);
         }
+
         [Test]
         public void TestUsernameLength()
         {
-            var options = new DbContextOptionsBuilder<MyDbContext>()
-                .UseInMemoryDatabase(databaseName: "TestDatabase")
-                .Options;
-
-            using (var context = new MyDbContext(options))
+            var user = new User
             {
-                var controller = new LoginController(context);
+                Username = "Userr",
+                Password = "Password"
+            };
 
+            var result = _controller.Create(user) as ViewResult;
 
-                var user = new User
-                {
-                    Username = "Userr",
-                    Password = "Password"
-                };
-
-                var result = controller.Create(user) as ViewResult;
-
-                Assert.IsNotNull(result);
-                Assert.IsNotNull(result.ViewData["ErrorMessage"]);
-                Assert.AreEqual("Username is too short. Your username must exceed 5 characters", result.ViewData["ErrorMessage"]);
-            }
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.ViewData["ErrorMessage"]);
+            Assert.AreEqual("Username is too short. Your username must exceed 5 characters", result.ViewData["ErrorMessage"]);
         }
+
         [Test]
         public void TestPasswordLength()
         {
-            var options = new DbContextOptionsBuilder<MyDbContext>()
-                .UseInMemoryDatabase(databaseName: "TestDatabase")
-                .Options;
-
-            using (var context = new MyDbContext(options))
+            var user = new User
             {
-                var controller = new LoginController(context);
+                Username = "DevelopmentProject",
+                Password = "Tester"
+            };
 
-               
+            var result = _controller.Create(user) as ViewResult;
 
-                var user = new User
-                {
-                    Username = "DevelopmentProject",
-                    Password = "Tester"
-                };
-
-                var result = controller.Create(user) as ViewResult;
-
-                Assert.IsNotNull(result);
-                Assert.IsNotNull(result.ViewData["ErrorMessage"]);
-                Assert.AreEqual("Password is too short. Your password must exceed 7 characters", result.ViewData["ErrorMessage"]);
-            }
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.ViewData["ErrorMessage"]);
+            Assert.AreEqual("Password is too short. Your password must exceed 7 characters", result.ViewData["ErrorMessage"]);
         }
+
         [Test]
         public void TestPasswordStrength()
         {
-            var options = new DbContextOptionsBuilder<MyDbContext>()
-                .UseInMemoryDatabase(databaseName: "TestDatabase")
-                .Options;
-
-            using (var context = new MyDbContext(options))
+            var user = new User
             {
-                var controller = new LoginController(context);
+                Username = "DevelopmentProject",
+                Password = "tester123"
+            };
 
-                var user = new User
-                {
-                    Username = "DevelopmentProject",
-                    Password = "tester123"
-                };
+            var result = _controller.Create(user) as ViewResult;
 
-                var result = controller.Create(user) as ViewResult;
-
-                Assert.IsNotNull(result);
-                Assert.IsNotNull(result.ViewData["ErrorMessage"]);
-                Assert.AreEqual("Password does not meet all of the requirements. Please try again", result.ViewData["ErrorMessage"]);
-            }
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.ViewData["ErrorMessage"]);
+            Assert.AreEqual("Password does not meet all of the requirements. Please try again", result.ViewData["ErrorMessage"]);
         }
+
         [Test]
         public void TestRedirectToLogin()
         {
-            var options = new DbContextOptionsBuilder<MyDbContext>()
-                .UseInMemoryDatabase(databaseName: "TestDatabase")
-                .Options;
+            _context.User.Add(new User { Username = "User", Password = "CorrectPassword" });
+            _context.SaveChanges();
 
-            using (var context = new MyDbContext(options))
+            var user = new User
             {
-                var controller = new LoginController(context);
+                Username = "Testing",
+                Password = "Testing123"
+            };
 
-                context.User.Add(new User { Username = "User", Password = "CorrectPassword" });
-                context.SaveChanges();
-
-                var user = new User
-                {
-                    Username = "Testing",
-                    Password = "Testing123"
-                };
-
-
-                var result = controller.Create(user);
-
-                var redirectResult = (RedirectToActionResult)result;
-                Assert.AreEqual("Index", redirectResult.ActionName);
-                Assert.IsNotNull(result);
-             
-            }
+            var result = _controller.Create(user);
+            var redirectResult = (RedirectToActionResult)result;
+            Assert.AreEqual("Index", redirectResult.ActionName);
+            Assert.IsNotNull(result);
         }
 
         private string HashPassword(string password)
         {
-
             return BCrypt.Net.BCrypt.HashPassword(password);
         }
     }
-
 }
